bce86c5ad64b4d1637532f21429eece5f24f860d
CarloLuis Rodriguez <crodriguez@carrasco.uruit.com>
2017-01-25 15:20:21 -0300

    examples.. (index)

--- src/index.js
+++ src/index.js
@@ -4,15 +4,26 @@ import SSuggestor from './components/suggestor/Suggestor';
 
 let suggestions = ['uno', 'unos', 'caña', 'araña', 'aa', 'ab', 'aab', 'aba'];
 
-const Sep = ({top=20}) => <div style={{paddingTop:top}} />
+const Sep = ({top=200}) => <div style={{paddingTop:top}} />;
+Sep.propTypes = {
+	top: React.PropTypes.number
+};
 
 ReactDOM.render(
-	<div>
+	<div style={{padding:20}}>
 		<h1>SSuggestor</h1>
-		<SSuggestor value="unos" list={suggestions} placeholder="type something..." style={{width:400}} onChange={value=>console.info(value)} />
-		<Sep top={200} />
-		<SSuggestor list={suggestions} placeholder="type something..." style={{width:400}} onChange={value=>console.info(value)} nox />
-		<Sep top={200} />
+		<pre>
+			{ `<Suggestor value="" list={suggestions} onChange={value=>console.info(value)} placeholder="type something..." style={{width:400}} arrow={true} nox={false} />` }
+		</pre>
+
+		<Sep top={20} />
+		<h4>Simple Suggestor</h4>
+		<SSuggestor value="unos" list={suggestions} onChange={value=>console.info(value)} placeholder="type something..." style={{width:400}} />
+		<Sep />
+		<SSuggestor list={suggestions} placeholder="suggestor 2..." style={{width:400}} onChange={value=>console.info(value)} nox />
+		<Sep />
+		<SSuggestor list={suggestions} placeholder="suggestor 3..." style={{width:400}} onChange={value=>console.info(value)} arrow={false} nox />
+		<Sep />
 	</div>,
 	document.getElementById('app')
 );
