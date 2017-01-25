ef4b34607e7742ab0794b74d160d874d7206a21a
CarloLuis Rodriguez <crodriguez@carrasco.uruit.com>
2017-01-25 15:07:07 -0300

    index - examples..

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import SSuggestor from './components/suggestor/Suggestor';
 
 let suggestions = ['uno', 'unos', 'caña', 'araña', 'aa', 'ab', 'aab', 'aba'];
 
+const Sep = ({top=20}) => <div style={{paddingTop:top}} />
+
 ReactDOM.render(
 	<div>
 		<h1>SSuggestor</h1>
-		<SSuggestor list={suggestions} placeholder="type something..." style={{width:400}} onChange={value=>console.info(value)} />
+		<SSuggestor value="unos" list={suggestions} placeholder="type something..." style={{width:400}} onChange={value=>console.info(value)} />
+		<Sep top={200} />
+		<SSuggestor list={suggestions} placeholder="type something..." style={{width:400}} onChange={value=>console.info(value)} nox />
+		<Sep top={200} />
 	</div>,
 	document.getElementById('app')
 );
