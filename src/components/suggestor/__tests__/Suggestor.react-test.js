0a73a50ab4616586707e499aa2e4c81c115dc5d7
carloluis <carloluisr@gmail.com>
2017-01-29 03:13:31 -0300

    suggestor - react-test * react-addons-test-utils

--- src/components/suggestor/__tests__/Suggestor.react-test.js
+++ src/components/suggestor/__tests__/Suggestor.react-test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Suggestor from '../Suggestor';
+import ReactTestUtils from 'react-addons-test-utils';
 
 describe('<Suggestor />', () => {
 	it('initial render with no suggestions', () => {
@@ -10,5 +11,21 @@ describe('<Suggestor />', () => {
 	it('initial render with suggestions', () => {
 		const tree = renderer.create(<Suggestor list={["suggest-1"]} />);
 		expect(tree).toMatchSnapshot();
-	})
+	});
+});
+
+describe('Suggestor', () => {
+	it('if suggestor value => open suggestions list', () => {
+		// arrange & precondition...
+		const component = ReactTestUtils.renderIntoDocument(<Suggestor list={['suggest-1', 'suggest-2']} />);
+		expect(component.refs.wrapped.state.open).toBeFalsy();
+
+		// act
+		let input = ReactTestUtils.findRenderedDOMComponentWithTag(component, 'input');
+		input.value = 'suggest';
+		ReactTestUtils.Simulate.change(input);
+
+		// expect
+		expect(component.refs.wrapped.state.open).toBeTruthy();
+	});
 });
