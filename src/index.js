abc0f95cc6ed0335d9f36e713489d3d762d23e3c
carloluis <carloluisr@gmail.com>
2017-01-31 01:41:25 -0300

    remove files...

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Example from './components/Example';
-
-ReactDOM.render(
-	<Example />, 
-	document.getElementById('example')
-);
