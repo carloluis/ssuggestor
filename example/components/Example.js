bd87f27552f6c8aedf678fa47cf318e38e56caf3
carloluis <carloluisr@gmail.com>
2017-02-06 01:17:12 -0300

    example - update + data

--- example/components/Example.js
+++ example/components/Example.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import SSuggestor, { Suggestor } from '../../src/suggestor/Suggestor';
 import Section from './Section';
+import { countries, suggestions, numbers } from '../data/index';
 
-const SUGGESTIONS = [
-	'a', 'aa', 'aaa', 'aaaa', 'aaaaa', 
-	'b', 'bb', 'bbb', 'bbbb', 'bbbbb',
-	'c', 'cc', 'ccc', 'cccc', 'ccccc',
-	'abc', 'aABC', 'AaBbCc',
-	'A', 'B', 'C'
-];
-const BIG_DATA = [];
-for (var i = 0; i < 2000; i++) {
-	BIG_DATA.push(''+i);
-}
 const handleSSuggestorChange = (value) => console.info(value);
 
 const STYLE_WIDTH = { width:'100%' };
@@ -22,61 +12,53 @@ const Example = () => (
 		<Section title="Code Example">
 		<pre>
 			{
-`<SSuggestor list={['list', 'of', 'suggestions', ...]} 
-	placeholder="type something..." 
-	style={{width:'100%''}} 
-	/>`
+`<SSuggestor list={['list', 'of', 'suggestions', ...]} style={{width:'100%''}} />`
 			}
 		</pre>	
 		</Section>
 
-		<Section title="Suggestor with initial value" description="Use `value` prop to set initial value. Hit `x` to delete current value. Use (up, down) keys to select values.">
-			<SSuggestor value="default" list={['list', 'of', 'suggestions', 'with', 'default', 'value']} 
-				onChange={handleSSuggestorChange} placeholder="..." style={STYLE_WIDTH} />
+		<Section title="Suggestor" description="Use `value` prop to set initial value. Click x-icon to delete current value. Use up, down, enter and escape keys.">
+			<SSuggestor value="default" list={suggestions} onChange={handleSSuggestorChange} placeholder="..." tooltip="type something.." style={STYLE_WIDTH} />
 		</Section>
 
-		<Section title="Suggestor with `select on tab`" description="Select element hovered pressing `tab`.">
-			<SSuggestor list={SUGGESTIONS} placeholder="type a|b|c..." onChange={handleSSuggestorChange} selectOnTab style={STYLE_WIDTH} />
+		<Section title="Suggestor with `selectOnTab` enabled" description="Select element hovered pressing `tab`. List of all countries.">
+			<SSuggestor list={countries} placeholder="countries..." tooltip="type a country name" 
+				onChange={handleSSuggestorChange} selectOnTab style={STYLE_WIDTH} />
 		</Section>
 		
-		<Section title="Suggestor with custom styles" description="Use `className` prop and css to change component appearance.">
-			<SSuggestor className="input-group suggestor" list={SUGGESTIONS} placeholder="type a|b|c..." 
-			onChange={handleSSuggestorChange} style={STYLE_WIDTH} />
+		<Section title="Suggestor with custom styles" description="Use className and style props to change component appearance.">
+			<SSuggestor className="input-group suggestor" list={suggestions} placeholder="type a letter or number..." style={STYLE_WIDTH} 
+				onChange={handleSSuggestorChange} />
 		</Section>
 
-		<Section title="Suggestor with tooltip" description="tooltip prop.">
-			<SSuggestor list={SUGGESTIONS} placeholder="type a|b|c..." tooltip="type a|b|c..."
-			onChange={handleSSuggestorChange} style={STYLE_WIDTH} />
+		<Section title="Suggestor without keys navigation" description="You can only type or use mouse to view/navigate over suggestions.">
+			<SSuggestor list={countries} placeholder="countries..." tooltip="no keys: type/scroll over list"
+				onChange={handleSSuggestorChange} style={STYLE_WIDTH} useKeys={false} />
 		</Section>
 
-		<Section title="Suggestor without keys navigation" description="useKeys prop.">
-			<SSuggestor list={SUGGESTIONS} placeholder="type a|b|c..." tooltip="no keys navigation"
-			onChange={handleSSuggestorChange} style={STYLE_WIDTH} useKeys={false} />
-		</Section>
-
-		<Section title="Suggestor without icons" description="arrow and x props.">
-			<SSuggestor list={SUGGESTIONS} placeholder="type a|b|c..." tooltip="no arrow and no x"
-			onChange={handleSSuggestorChange} style={STYLE_WIDTH} arrow={false} x={false} />
+		<Section title="Suggestor without icons" description="No arrow nor close icon. ">
+			<SSuggestor list={suggestions} placeholder="type a letter or number..." tooltip="no arrow and no x"
+			onChange={handleSSuggestorChange} style={STYLE_WIDTH} arrow={false} close={false} />
 		</Section>
 
 		<Section title="Suggestor start suggestions on two characters" description="suggestOn prop.">
-			<SSuggestor list={SUGGESTIONS} placeholder="type a|b|c..." tooltip="display suggestions on 2nd char."
-			onChange={handleSSuggestorChange} style={STYLE_WIDTH} suggestOn={2} />
+			<SSuggestor list={countries} placeholder="countries..." tooltip="display suggestions on 2nd char"
+				onChange={handleSSuggestorChange} style={STYLE_WIDTH} suggestOn={2} />
 		</Section>
 
 		<Section title="Suggestor without click support" description="openOnClick prop.">
-			<SSuggestor list={SUGGESTIONS} placeholder="type a|b|c..." tooltip="type something (or use navigation keys) to display suggestions"
+			<SSuggestor list={suggestions} placeholder="type a letter or number..." tooltip="type something (or use navigation keys) to display suggestions"
 			onChange={handleSSuggestorChange} style={STYLE_WIDTH} openOnClick={false} />
 		</Section>
 
-		<Section title="Suggestor with 2K suggestions" description="Long suggestion list... (using keys, waiting until 2nd char for suggestions)">
-			<SSuggestor list={BIG_DATA} placeholder="enter two digits..." tooltip="numbers between 0 and 2000. (no arrow, no x, no keys)" 
-				useKeys={true} suggestOn={2} openOnClick={false} arrow={false} x={false}
+		<Section title="Suggestor with 2K suggestions" description="Long suggestion list (of numbers). Disable keys support and open on click. Waiting until 2nd char for suggestions.">
+			<SSuggestor list={numbers} placeholder="enter two digits..." tooltip="numbers between 0 and 2000. (no icons, no keys, no click)" 
+				useKeys={false} suggestOn={2} openOnClick={false} arrow={false} close={false}
 				style={STYLE_WIDTH} onChange={handleSSuggestorChange} />
 		</Section>
 
-		<Section title="Suggestor without clickout support" description="This one doesn`t close on click outside component.">
-			<Suggestor list={SUGGESTIONS} placeholder="type a|b|c..." tooltip="only support clicks on component" 
+		<Section title="Suggestor without clickout support" description="This one doesn`t close on click outside component. Only support clicks on component.">
+			<Suggestor list={suggestions} placeholder="type a letter or number..." tooltip="only support clicks on component" 
 				style={STYLE_WIDTH} onChange={handleSSuggestorChange} />
 		</Section>
 	</div>
---
+++ example/data/index.js
@@ -0,0 +1,17 @@
+export { countries } from './countries.js';
+
+export const suggestions = [
+	'list of', 'suggestions', 'with', 'default', 'value', '...', 'javascript', 'react', 'ssuggestor',
+	'abcdefghijklmnopqrstuvwxyz', 'abcdef', 'ghijkl', 'mnopqr', 'stuvwx', 'yzabcd', 'ABCDEF',
+	'01234567890', '01234', '56789', '11235', '3.14159265', '2.7182'
+];
+
+export const getNumbersList = (init=0, end=100) => {
+	let numbers = [];
+	for (let i = init; i < end; i++) {
+		numbers.push(''+i);
+	}
+	return numbers;
+};
+
+export const numbers = getNumbersList(0, 2000);
