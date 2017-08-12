4047b9a97c60ca245323dabd5aaa8dae6193a13e
carloluis <carloluisr@gmail.com>
2017-08-12 17:39:24 -0300

    add styles mock

---
+++ src/suggestor/__mocks__/styles.js
@@ -0,0 +1,7 @@
+const styles = require.requireActual('../styles');
+
+export const SPIN_STYLES = styles.SPIN_STYLES;
+
+export const X_STYLES = styles.X_STYLES;
+
+export const getListStyles = jest.fn(styles.getListStyles);
