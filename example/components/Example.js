e4e0ca7388b0952a2820795a330c58b1311886ea
carloluis <carloluisr@gmail.com>
2018-02-11 11:53:49 -0300

    update suggestor examples..

--- example/components/Example.js
+++ example/components/Example.js
@@ -4,12 +4,22 @@ import ExUpdate from './ExUpdate';
 import Section from './Section';
 import { countries, suggestions, numbers } from '../data/index';
 
-const handleSuggestorChange = value => console.info('change value to:', value);
-const valueChanged = (value, item) => console.info('%s : %o', value, item);
+const valueSelected = (value, item) => console.info('%s : %o', value, item);
+const valueChanged = value => console.info('change value to:', value);
 
-const STYLE_WIDTH = { width: '100%' };
 const suggestionObjects = [{ x: 'One', y: 1 }, { x: 'Two', y: 2 }];
 const selector = item => `${item.x}:${item.y}`;
+
+const SuggestorWrapper = props => (
+	<Suggestor
+		style={{ width: '100%' }}
+		onChange={valueChanged}
+		onSelect={valueSelected}
+		placeholder="..."
+		{...props}
+	/>
+);
+
 /* eslint-disable max-len */
 
 const Example = () => (
@@ -20,56 +30,31 @@ const Example = () => (
 		</Section>
 
 		<Section title="Using suggestion objects" description="Use selector prop to display suggestions">
-			<Suggestor
-				list={suggestionObjects}
-				selector={selector}
-				onChange={handleSuggestorChange}
-				onSelect={valueChanged}
-				placeholder="..."
-				style={STYLE_WIDTH}
-				selectOnTab
-			/>
+			<SuggestorWrapper list={suggestionObjects} selector={selector} selectOnTab />
 		</Section>
 
 		<Section
 			title="Suggestor"
 			description="Use `value` prop to set initial value. Use up, down, enter and escape keys."
 		>
-			<Suggestor
-				value="default"
-				list={suggestions}
-				onChange={handleSuggestorChange}
-				onSelect={valueChanged}
-				placeholder="..."
-				tooltip="type something.."
-				style={STYLE_WIDTH}
-			/>
+			<SuggestorWrapper value="default" list={suggestions} tooltip="type something.." />
 		</Section>
 
 		<Section
 			title="Suggestor with `selectOnTab` enabled"
 			description="Select element hovered pressing `tab`. List of all countries."
 		>
-			<Suggestor
-				list={countries}
-				placeholder="countries..."
-				tooltip="type a country name"
-				onChange={handleSuggestorChange}
-				selectOnTab
-				style={STYLE_WIDTH}
-			/>
+			<SuggestorWrapper list={countries} placeholder="countries..." tooltip="type a country name" selectOnTab />
 		</Section>
 
 		<Section
 			title="Suggestor with custom styles"
 			description="Use className and style props to change component appearance."
 		>
-			<Suggestor
+			<SuggestorWrapper
 				className="input-group suggestor"
 				list={suggestions}
 				placeholder="type a letter or number..."
-				style={STYLE_WIDTH}
-				onChange={handleSuggestorChange}
 			/>
 		</Section>
 
@@ -77,46 +62,38 @@ const Example = () => (
 			title="Suggestor without keys navigation"
 			description="You can only type or use mouse to view/navigate over suggestions."
 		>
-			<Suggestor
+			<SuggestorWrapper
 				list={countries}
 				placeholder="countries..."
 				tooltip="no keys: type/scroll over list"
-				onChange={handleSuggestorChange}
-				style={STYLE_WIDTH}
 				useKeys={false}
 			/>
 		</Section>
 
 		<Section title="Suggestor without icons" description="No arrow nor close icon. ">
-			<Suggestor
+			<SuggestorWrapper
 				list={suggestions}
 				placeholder="type a letter or number..."
 				tooltip="no arrow and no x"
-				onChange={handleSuggestorChange}
-				style={STYLE_WIDTH}
 				arrow={false}
 				close={false}
 			/>
 		</Section>
 
 		<Section title="Suggestor start suggestions on two characters" description="suggestOn prop.">
-			<Suggestor
+			<SuggestorWrapper
 				list={countries}
 				placeholder="countries..."
 				tooltip="display suggestions on 2nd char"
-				onChange={handleSuggestorChange}
-				style={STYLE_WIDTH}
 				suggestOn={2}
 			/>
 		</Section>
 
 		<Section title="Suggestor without click support" description="openOnClick prop.">
-			<Suggestor
+			<SuggestorWrapper
 				list={suggestions}
 				placeholder="type a letter or number..."
 				tooltip="type something (or use navigation keys) to display suggestions"
-				onChange={handleSuggestorChange}
-				style={STYLE_WIDTH}
 				openOnClick={false}
 			/>
 		</Section>
@@ -125,7 +102,7 @@ const Example = () => (
 			title="Suggestor with 2K suggestions (numbers)"
 			description="Disable keys support and open on click props. Waiting until 2nd char to display suggestions."
 		>
-			<Suggestor
+			<SuggestorWrapper
 				list={numbers}
 				placeholder="enter two digits..."
 				tooltip="numbers between 0 and 2000. (no icons, no keys, no click)"
@@ -134,8 +111,6 @@ const Example = () => (
 				openOnClick={false}
 				arrow={false}
 				close={false}
-				style={STYLE_WIDTH}
-				onChange={handleSuggestorChange}
 			/>
 		</Section>
 
