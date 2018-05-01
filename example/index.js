d4b4b400bba420a07ddc3bd8fb7fab458bfe0834
Carloluis Rodriguez <carloluisr@gmail.com>
2018-04-30 21:30:14 -0300

    feature/css-styles (#50)
    * configure css and style loaders (dev)
    
    * extracting css on prod. build
    
    * codacy feedback: - Expected RPAREN
    
    * update index.css

--- example/index.js
+++ example/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Example from './components/Example';
+import './index.css';
 
-ReactDOM.render(
-	<Example />,
-	document.getElementById('example')
-);
+ReactDOM.render(<Example />, document.getElementById('example'));
