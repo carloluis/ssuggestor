0802a2dbfafe69a95bdd451fa7837382cbd82a98
carloluis <carloluisr@gmail.com>
2018-05-03 21:14:12 -0300

    update suggestor example app (markup/styles) - clean up index.html

---
+++ example/components/app/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Example from '../example/Example';
+import styles from './app.css';
+
+const App = () => (
+	<React.Fragment>
+		<header className={styles.header}>
+			<div className={styles.title}>
+				<h1>{'<ssuggestor>'}</h1>
+				<h2>React Simple Suggestor</h2>
+			</div>
+			<a
+				className={styles.link}
+				href="https://github.com/carloluis/ssuggestor"
+				target="_blank"
+				rel="noopener noreferrer"
+			>
+				Code and Docs on GitHub
+			</a>
+		</header>
+		<section className={styles.container}>
+			<Example />
+		</section>
+		<footer className={styles.footer}>
+			<span>
+				MIT &copy;{' '}
+				<a href="https://twitter.com/carloluis_" target="_blank" rel="noopener noreferrer">
+					Carloluis
+				</a>{' '}
+				2017
+			</span>
+		</footer>
+	</React.Fragment>
+);
+
+export default App;
--- example/index.js
+++ example/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Example from './components';
+import App from './components';
 import './index.css';
 
-ReactDOM.render(<Example />, document.getElementById('example'));
+ReactDOM.render(<App />, document.getElementById('app'));
