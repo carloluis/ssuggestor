8cbbbdfbe24400d76545be4dd72fee5bb85eaca2
carloluis <carloluisr@gmail.com>
2018-05-10 12:53:25 -0300

    update project example

--- example/index.jsx
+++ example/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components';
+import './../src/suggestor/styles/index.scss';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('app'));
