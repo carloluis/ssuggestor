e72c271750e21f94308e4f9ad841870077100ad9
carloluis <carloluisr@gmail.com>
2018-05-18 00:27:32 -0300

    add bootstrap 3-4 classes schemas

---
+++ src/class-schema/bootstrap-3.js
@@ -0,0 +1,11 @@
+// Bootstrap 3 scheme classes
+
+export default {
+	root: 'input-group',
+	input: 'form-control',
+	arrow: 'glyphicon glyphicon-triangle-bottom ss-triangle',
+	close: 'glyphicon glyphicon-remove ss-remove',
+	list: 'dropdown-menu ss-list',
+	item: '',
+	activeItem: 'ss-over-item'
+};
---
+++ src/class-schema/index.js
@@ -0,0 +1,5 @@
+import bootstrap3 from './bootstrap-3';
+import bootstrap4 from './bootstrap-4';
+
+export { bootstrap3 };
+export { bootstrap4 };
