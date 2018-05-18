770ed4b30c78d76d8be3ad77479adfe66c5fc0f8
carloluis <carloluisr@gmail.com>
2018-05-18 00:11:20 -0300

    remove js styles mock

--- src/suggestor/__mocks__/styles.js
+++
@@ -1,7 +0,0 @@
-const styles = require.requireActual('../styles');
-
-export const SPIN_STYLES = styles.SPIN_STYLES;
-
-export const X_STYLES = styles.X_STYLES;
-
-export const getListStyles = jest.fn(styles.getListStyles);
